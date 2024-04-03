989f88759ab248f51b4a4b578a61b25888c05b90
as2305 <165639720+as2305@users.noreply.github.com>
2024-04-03 01:40:33 -0400

    Add BMI calculator

--- OpenBodyCalc.py
+++ OpenBodyCalc.py
@@ -1,21 +1,36 @@
 from customtkinter import *
 
 app = CTk()
-app.geometry("900x600")
+app.geometry("600x600")
 app.title("OpenBodyCalc")
 
 tabview = CTkTabview(master=app)
 
-tabview.pack(padx=20, pady=20)
+tabview.pack(fill = BOTH, expand = True)
 
 tabview.add("BMI")
 tabview.add("Tab 2")
 tabview.add("Tab 3")
 
-label = CTkLabel(master=tabview.tab("BMI"), text="test")
-label.pack(padx=20, pady=20)
+def test():
+    bmi_calculation = float(weight1.get()) / (float(height1.get())*float(height1.get()))
+    bmi.configure(text="Your BMI is "+str(bmi_calculation))
 
-height1 = CTkEntry(master=tabview.tab("BMI"), placeholder_text="Enter your height...")
-height1.pack(padx=30, pady=30)
+label = CTkLabel(master=tabview.tab("BMI"), text="BMI")
+label.place(relx=0.5, rely=0.3, anchor="center")
+
+height1 = CTkEntry(master=tabview.tab("BMI"), placeholder_text="Height in meter")
+height1.place(relx=0.5, rely=0.4, anchor="center")
+
+weight1 = CTkEntry(master=tabview.tab("BMI"), placeholder_text="Weight in kgs")
+weight1.place(relx=0.5, rely=0.5, anchor="center")
+
+calculate1 = CTkButton(master=tabview.tab("BMI"), text="Calculate", command=test)
+calculate1.place(relx=0.5, rely=0.6, anchor="center")
+
+
+
+bmi = CTkLabel(master=tabview.tab("BMI"), text=None)
+bmi.place(relx=0.5, rely=0.7, anchor="center")
 
 app.mainloop()
