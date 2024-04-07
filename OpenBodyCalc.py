3fc537f69a75891333342a7423d973e054520774
as2305 <165639720+as2305@users.noreply.github.com>
2024-04-07 13:17:39 -0400

    Improve widget positioning

--- OpenBodyCalc.py
+++ OpenBodyCalc.py
@@ -1,7 +1,7 @@
 from customtkinter import *
 
 app = CTk()
-app.geometry("600x600")
+app.geometry("500x500")
 app.title("OpenBodyCalc")
 
 tabview = CTkTabview(master=app)
@@ -34,16 +34,16 @@ def CalcBMI():
         bmi_indicator.configure(text="OBESE", text_color="red")
 
 label1 = CTkLabel(master=tabview.tab("BMI"), text="BMI")
-label1.place(relx=0.5, rely=0.3, anchor="center")
+label1.place(relx=0.5, rely=0.2, anchor="center")
 
 height1 = CTkEntry(master=tabview.tab("BMI"), placeholder_text="Height (meters)")
-height1.place(relx=0.5, rely=0.4, anchor="center")
+height1.place(relx=0.5, rely=0.3, anchor="center")
 
 weight1 = CTkEntry(master=tabview.tab("BMI"), placeholder_text="Weight (kgs)")
-weight1.place(relx=0.5, rely=0.5, anchor="center")
+weight1.place(relx=0.5, rely=0.4, anchor="center")
 
 calculate1 = CTkButton(master=tabview.tab("BMI"), text="Calculate", command=CalcBMI)
-calculate1.place(relx=0.5, rely=0.6, anchor="center")
+calculate1.place(relx=0.5, rely=0.5, anchor="center")
 
 bmi = CTkLabel(master=tabview.tab("BMI"), text=None)
 bmi.place(relx=0.5, rely=0.7, anchor="center")
@@ -72,14 +72,14 @@ bmr_weight = CTkEntry(master=tabview.tab("BMR"), placeholder_text=("Weight in kg
 calcBmrButton = CTkButton(master=tabview.tab("BMR"), text="Calculate", command=calcBmr)
 bmrOutput = CTkLabel(master=tabview.tab("BMR"), text=None)
 
-bmr_label.place(relx=0.5, rely=0.3, anchor="center")
-bmr_age.place(relx=0.5, rely=0.4, anchor="center")
-male_1.place(relx=0.5, rely=0.5, anchor="center")
-female_2.place(relx=0.6, rely=0.5, anchor="center")
-bmr_height.place(relx=0.5, rely=0.6, anchor="center")
-bmr_weight.place(relx=0.5, rely=0.7, anchor="center")
-calcBmrButton.place(relx=0.5, rely=0.8, anchor="center")
-bmrOutput.place(relx=0.5, rely=0.9, anchor="center")
+bmr_label.place(relx=0.5, rely=0.2, anchor="center")
+bmr_age.place(relx=0.5, rely=0.3, anchor="center")
+male_1.place(relx=0.45, rely=0.4, anchor="center")
+female_2.place(relx=0.6, rely=0.4, anchor="center")
+bmr_height.place(relx=0.5, rely=0.5, anchor="center")
+bmr_weight.place(relx=0.5, rely=0.6, anchor="center")
+calcBmrButton.place(relx=0.5, rely=0.7, anchor="center")
+bmrOutput.place(relx=0.5, rely=0.8, anchor="center")
 
 
 #HRMax
@@ -89,13 +89,13 @@ def CalcHRMax():
 
 
 label2 = CTkLabel(master=tabview.tab("HRMAX"), text="Heart Rate Max")
-label2.place(relx=0.5, rely=0.3, anchor="center")
+label2.place(relx=0.5, rely=0.2, anchor="center")
 age = CTkEntry(master=tabview.tab("HRMAX"), placeholder_text="Age")
-age.place(relx=0.5, rely=0.4, anchor="center")
+age.place(relx=0.5, rely=0.3, anchor="center")
 calculate2 = CTkButton(master=tabview.tab("HRMAX"), text="Calculate", command=CalcHRMax) 
-calculate2.place(relx=0.5, rely=0.6, anchor="center")
+calculate2.place(relx=0.5, rely=0.4, anchor="center")
 hrm = CTkLabel(master=tabview.tab("HRMAX"), text=None)
-hrm.place(relx=0.5, rely=0.7, anchor="center")
+hrm.place(relx=0.5, rely=0.5, anchor="center")
 
 #VO2MAX
 
@@ -104,11 +104,11 @@ def CalcVO2MAX():
     vo2m.configure(text="Your VO2Max is "+str(round(vo2,1)))
 
 label3 = CTkLabel(master=tabview.tab("VO2MAX"), text="VO2Max (Cooper Test)")
-label3.place(relx=0.5, rely=0.3, anchor="center")
+label3.place(relx=0.5, rely=0.2, anchor="center")
 distance = CTkEntry(master=tabview.tab("VO2MAX"), placeholder_text="distance run in 12 minutes (meter)")
-distance.place(relx=0.5, rely=0.4, anchor="center")
+distance.place(relx=0.5, rely=0.3, anchor="center")
 calculate3 = CTkButton(master=tabview.tab("VO2MAX"), text="Calculate", command=CalcVO2MAX)
-calculate3.place(relx=0.5, rely=0.6, anchor="center")
+calculate3.place(relx=0.5, rely=0.4, anchor="center")
 vo2m = CTkLabel(master=tabview.tab("VO2MAX"), text=None)
 vo2m.place(relx=0.5, rely=0.7, anchor="center")
 
@@ -127,12 +127,12 @@ def radiobutton_event():
         set_appearance_mode("dark")
 
 themelabel = CTkLabel(master=tabview.tab("Settings"), text="Theme: ")
-themelabel.place(relx=0.3, rely=0.5, anchor="center")
-radio_var = IntVar(value=0)
+themelabel.place(relx=0.3, rely=0.2, anchor="center")
+radio_var = IntVar(value=1)
 radiobutton_1 = CTkRadioButton(master=tabview.tab("Settings"), text="Light",command=radiobutton_event, variable= radio_var, value=1)
 radiobutton_2 = CTkRadioButton(master=tabview.tab("Settings"), text="Dark",command=radiobutton_event, variable= radio_var, value=2)
-radiobutton_1.place(relx=0.5, rely=0.5, anchor="center")
-radiobutton_2.place(relx=0.7, rely=0.5, anchor="center")
+radiobutton_1.place(relx=0.5, rely=0.2, anchor="center")
+radiobutton_2.place(relx=0.7, rely=0.2, anchor="center")
 
 fullscreenlabel = CTkLabel(master=tabview.tab("Settings"), text="Fullscreen: ")
 def switch_event():
@@ -141,9 +141,9 @@ def switch_event():
         app.attributes("-fullscreen", "True")
     elif stat == "off":
         app.attributes("-fullscreen", "False")
-switch_var = StringVar(value="off")
+switch_var = StringVar(value="off_")
 switch = CTkSwitch(master=tabview.tab("Settings"), text=None, command=switch_event,variable=switch_var, onvalue="on", offvalue="off")
-fullscreenlabel.place(relx=0.3, rely=0.7, anchor="center")
-switch.place(relx=0.5, rely=0.7, anchor="center")
+fullscreenlabel.place(relx=0.3, rely=0.3, anchor="center")
+switch.place(relx=0.5, rely=0.3, anchor="center")
 
 app.mainloop()
